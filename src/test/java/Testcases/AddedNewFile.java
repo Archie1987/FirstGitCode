7e93602782a0518ee87e47f5aaac9a7debc74a36
Archie1987 <archies.mishra@gmail.com>
2022-12-08 18:25:42 +0530

    changes done

---
+++ src/test/java/Testcases/AddedNewFile.java
@@ -0,0 +1,15 @@
+package Testcases;
+
+public class AddedNewFile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	System.out.println("Adding into git");	
+	
+	
+	
+	
+	}
+
+}
